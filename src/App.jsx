40198cc6e1dc68297a84cbd547b136a0fb6a06a5
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-07-19 21:18:00 +0100

    feat: add Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Nav from "./components/nav/Nav";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
 }
 
 export default App;
