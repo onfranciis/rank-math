8e6456dd542af5635e64c9e6598e25b9cf175fb1
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-07-21 00:53:23 +0100

    feat: add Part3

--- src/components/wallet/Wallet.jsx
+++ src/components/wallet/Wallet.jsx
@@ -1,5 +1,6 @@
 import Part1 from "./Part1";
 import Part2 from "./Part2";
+import Part3 from "./Part3";
 import "./Wallet.css";
 
 const Wallet = () => {
@@ -7,6 +8,7 @@ const Wallet = () => {
     <div className="Wallet">
       <Part1 />
       <Part2 />
+      <Part3 />
     </div>
   );
 };
