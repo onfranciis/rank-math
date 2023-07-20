46e17219c5c4129e196e987dcfa51e4f2e209ef1
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-07-20 21:02:21 +0100

    feat: create Part1

---
+++ src/components/wallet/Part1.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import {
+  BsChevronCompactDown,
+  BsChevronCompactUp,
+  BsCurrencyBitcoin,
+} from "react-icons/bs";
+
+const Part1 = () => {
+  const [dropdown, setDropdown] = useState(false);
+
+  return (
+    <div className="Part1">
+      <div className="Top">
+        <div className="Coin">
+          <BsCurrencyBitcoin className="Icon" />
+        </div>
+
+        <p className="Currency">Bitcoin</p>
+
+        <p className="SubCurrency">BTC</p>
+      </div>
+
+      <p className="Body">3.529020 BTC</p>
+
+      <div className="Bottom">
+        <p className="Price">$19.153 USD</p>
+
+        <p className="Percentage">-2.32%</p>
+      </div>
+
+      <div className="Extra" style={{ display: dropdown ? "flex" : "none" }}>
+        <button className="Buy" title="Buy">
+          Buy
+        </button>
+
+        <button className="Sell" title="Sell">
+          Sell
+        </button>
+      </div>
+
+      <button className="Toggle" onClick={() => setDropdown(!dropdown)}>
+        {dropdown ? (
+          <BsChevronCompactUp className="Arrow" />
+        ) : (
+          <BsChevronCompactDown className="Arrow" />
+        )}
+      </button>
+    </div>
+  );
+};
+
+export default Part1;
