29e789048d7b1b307310ad1dd5a47f9be060baed
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-07-20 21:02:53 +0100

    feat: create Wallet

---
+++ src/components/wallet/Wallet.jsx
@@ -0,0 +1,12 @@
+import Part1 from "./Part1";
+import "./Wallet.css";
+
+const Wallet = () => {
+  return (
+    <div className="Wallet">
+      <Part1 />
+    </div>
+  );
+};
+
+export default Wallet;
