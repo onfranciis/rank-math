9922ea62dc98d4b16644e0d88d8d94def665f500
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-07-21 01:49:47 +0100

    fix: update styles

---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,12 @@
+import { BsChevronLeft } from "react-icons/bs";
+import "./Menu.css";
+
+const Menu = () => {
+  return (
+    <div className="Menu">
+      <BsChevronLeft />
+    </div>
+  );
+};
+
+export default Menu;
