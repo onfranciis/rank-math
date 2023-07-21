fd57c6f72843f202defb522c043566d0c9a82e17
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-07-21 09:32:25 +0100

    feat: add Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Menu from "./components/menu/Menu";
 import Nav from "./components/nav/Nav";
 import Wallet from "./components/wallet/Wallet";
 
 function App() {
   return (
     <div className="App">
+      <Menu />
       <Wallet />
       <Nav />
     </div>
